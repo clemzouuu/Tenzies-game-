91e9398f98ea1bf43acfd5b92e8a08982329f4bc
clemzouuu <92720413+clemzouuu@users.noreply.github.com>
2022-09-05 11:44:52 +0200

    not finished

--- tenzies/components/Die.jsx
+++ tenzies/components/Die.jsx
@@ -1,6 +1,13 @@
 export default function Die (props) {
+    let isItHeld = {
+      background: props.isItHeld ? 'green' : '',
+    }
+
     return (
-      <div className="die-face">
+      <div className="die-face"  
+      onClick={props.toggle}
+      style={isItHeld}
+      >
         <p className="die-num">{props.value}</p>
       </div>
     )
--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,32 +1,49 @@
 import { useState } from 'react'
 import './App.css'
-import Die from '../components/Die'
+import Die from '../components/Die' 
 
 function App() {
   
 
   const [dice,setDice] = useState(allNewDice())
-
+  
   function allNewDice() {
     let numberArray = []
     for(let count = 0; count < 10; count ++){
-     numberArray.push(Math.floor(Math.random() * 6))
+    
+      numberArray.push({
+        value:Math.floor(Math.random() * 6),
+        isHeld:true,
+        }
+      )
     }
     return numberArray
   } 
-  console.log(dice)
    
   const eachDie = dice.map(
     number => 
-    <Die value={number} />
+    <Die value={number.value} 
+    toggle={trueOrFalse}
+    />
   )
+
+  function trueOrFalse (){
+     console.log("cc")
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+
 
   return (
     <div className="App">
       <main>
-        <div className="dice-container">
-          {eachDie}
-        </div>
+        <div className="dice-container">{eachDie}</div>
+        <button className="roll-dice" onClick={rollDice}>
+          Roll dice
+        </button>
       </main>
     </div>
   )
